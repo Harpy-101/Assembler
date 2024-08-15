e2478ed2391908f04e1b8826db2f914d510cfe4d
David Israel <davidinp6@gmail.com>
2024-08-16 02:58:40 +0300

    Small changes to the functions

--- panic.c
+++ panic.c
@@ -1,6 +1,16 @@
 #include "panic.h"
-
+#include "ast.h"
 
 void memory_allocation_failure() {
-    printf("\033[31mpanic!\033[0m failure to allocate memory.\n");
+    printf("\033[31mpanic!\033[0m memory alocation failed.\n");
+    exit(EXIT_FAILURE);
+}
+
+void too_many_arguments(Token* token, int arg_count) {
+    printf("\033[31mpanic!\033[0m at line: %d an instruction of type: %s can only hold %d argumentas\n", token->line, token->val, arg_count);
+}
+
+void ASTNode_error(ASTNode* node) {
+    printf("\033[31mpanic!\033[0m at line: %d: %s\n", node->line, node->data.status.error_code);
+    exit(EXIT_FAILURE);
 }
--- panic.h
+++ panic.h
@@ -2,7 +2,11 @@
 #define PANIC_H
 
 #include <stdio.h>
+#include "lexer.h"
+#include "ast.h"
 
 void memory_allocation_failure();
+void too_many_arguments(Token* token, int arg_count);
+void ASTNode_error(ASTNode* node);
 
 #endif
