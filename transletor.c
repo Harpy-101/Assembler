d3f876ea6b817f8fd8aef5ef8c080ecc294c1e96
David Israel <davidinp6@gmail.com>
2024-08-17 08:37:04 +0300

    Added handling functions for the instructions and some of the directives. Also added printing functions for testing

--- transletor.c
+++ transletor.c
@@ -3,18 +3,33 @@
 #include "lexer.h"
 #include "panic.h"
 #include "symbol_table.h"
-#include <cstdio>
+#include <string.h>
 
-// Creata a function that can send the node divide a into helper functions that can handke it's conetents
-void delegate_node(ASTNode* node, char* file_name) {
+Word* word_arr[3] = {0,0,0};
+
+/* Creata a function that can send the node divide a into helper functions that can handke it's conetents */
+void delegate_node(ASTNode* node, char* file_name, Shed* shed) {
     while (node->next != NULL) {
         if (node->type == AST_INSTRUCTION) {
             /* Make sure all alocations were checked! */
             firstWord* first_word = malloc(sizeof(firstWord));
+            translate_instruction_first(first_word, node, shed);
             node = node->next;
         }
         else if (node->type == AST_DIRECTIVE) {
-            
+            if (strcmp(node->data.directive.directive, ".entry") == 0) {
+                return;
+            }
+            else if (strcmp(node->data.directive.directive, ".extern") == 0) {
+                return;
+            }
+            else if (strcmp(node->data.directive.directive, ".data") == 0) {
+                translate_data_directive(node, shed);
+            }
+            else if (strcmp(node->data.directive.directive, ".string") == 0) {
+                translate_string_directive(node, shed);
+                node = node->next;
+            }
         }
         else if (node->type == AST_LABEL) {
         
@@ -22,25 +37,35 @@ void delegate_node(ASTNode* node, char* file_name) {
     }
 }
 
-// Creata a function that can translate an instruction
+/* Creata a function that can translate an instruction */
 firstWord* translate_instruction_first(firstWord* first_word ,ASTNode* node, Shed* shed) {
     int instruction_opcode;
     int source_addressing_mode = IGNORE;
     int destenation_addressing_mode = IGNORE;
+    int source_value;
+    int destenation_value;
 
-    first_word = 0;
+    memset(first_word, 0, sizeof(firstWord));
     instruction_opcode = id_opcode(node->data.instruction.name);
-    if (node->data.instruction.arg1 != NULL) {
-        source_addressing_mode = id_addressing_mode(node->data.instruction.arg1_addressing_mode);
-        first_word->source_address = source_addressing_mode;
-    }
+
     if (node->data.instruction.arg2 != NULL) {
-        destenation_addressing_mode = id_addressing_mode(node->data.instruction.arg2_addressing_mode);
-        first_word->destenation_address = destenation_addressing_mode;
+        source_addressing_mode = node->data.instruction.arg1_addressing_mode;
+        source_value = id_addressing_mode(node->data.instruction.arg1_addressing_mode);
+        first_word->source_address = source_value;
+        destenation_addressing_mode = node->data.instruction.arg2_addressing_mode;
+        destenation_value = id_addressing_mode(node->data.instruction.arg2_addressing_mode);
+        first_word->destenation_address = destenation_value;
+    }
+    else if (node->data.instruction.arg1 != NULL) {
+        source_addressing_mode = node->data.instruction.arg1_addressing_mode;
+        source_value = id_addressing_mode(node->data.instruction.arg1_addressing_mode);
+        first_word->destenation_address = source_value;
     }
 
-    first_word->are = 1;
+    first_word->are = 4;
     first_word->opcode = instruction_opcode;
+
+    print_first_word(first_word);
 
     /* an instruction that has at least one operand */
     if (source_addressing_mode != IGNORE) {
@@ -51,22 +76,33 @@ firstWord* translate_instruction_first(firstWord* first_word ,ASTNode* node, She
                if (register_word == NULL) {
                 memory_allocation_failure();
             }
-            translate_indirect_register(register_word, node->data.instruction.arg1, SOURCE);
+            if (destenation_addressing_mode == IGNORE) {
+                translate_indirect_register(register_word, node->data.instruction.arg1, DESTENATION);
+            }
+            else {
+                translate_indirect_register(register_word, node->data.instruction.arg1, SOURCE);
+            }
                /* Handle the second word */
                if (destenation_addressing_mode != IGNORE) {
+                shed->register_word = register_word;
                 handle_second_word(node, source_addressing_mode, destenation_addressing_mode, shed);
             }
+            print_register_word(register_word);
+            return first_word;
          }
         /* First operand is a label */
+
+
         else if (source_addressing_mode == DIRECT) {
             Word* word = malloc(sizeof(Word));
             if (word == NULL) {
                 memory_allocation_failure();
            }
-            translate_direct(word, node->data.label.name, address, shed);
+            translate_direct(&word->type.direct_word, node->data.label.name, *shed->ic, shed);
             if (destenation_addressing_mode != IGNORE) {
                 handle_second_word(node, source_addressing_mode, destenation_addressing_mode, shed);
             }
+            return first_word; 
         }
         /* First operand is a number */
         else if (source_addressing_mode == IMIDIATE) {
@@ -74,10 +110,11 @@ firstWord* translate_instruction_first(firstWord* first_word ,ASTNode* node, She
             if (word == NULL) {
                 memory_allocation_failure();
             }
-            translate_imidiate(word, node->data.imidiate_value.value);
+            translate_imidiate(&word->type.imidiate_word, node->data.imidiate_value.value);
             if (destenation_addressing_mode != IGNORE) {
                 handle_second_word(node, source_addressing_mode, destenation_addressing_mode, shed);
             }
+            return first_word;
         }
     }    
 
@@ -95,7 +132,7 @@ void handle_second_word(ASTNode* node, int source_addressing_mode, int destenati
         if (direct_word == NULL) {
             memory_allocation_failure();
         }
-        translate_direct(direct_word, node->data.label.name, address, shed->file_name, shed->unresolved_list);
+        translate_direct(direct_word, node->data.label.name, *shed->ic, shed);
     }
     /* Second operand is a directive */
     if (destenation_addressing_mode == IMIDIATE) {
@@ -146,7 +183,7 @@ int id_addressing_mode(int addressing_mode) {
 
 void translate_direct(directWord* word, char* label_name, int address, Shed* shed) {
     Symbol* found;
-    found  = lookup_symbol(symbol_table, label_name);
+    found  = lookup_symbol(shed->symbol_table, label_name);
     if (found == NULL) {
         add_unresolved_label(label_name, address, shed->unresolved_list, shed->file_name);
     }
@@ -155,7 +192,7 @@ void translate_direct(directWord* word, char* label_name, int address, Shed* she
     
     /* create an if statment that checks if a label was defined in the current file 
        based on that sewt the "are" portiion of the word*/
-    word->are = 1;
+    word->are = 4;
     word->address = found->address;
 
 }
@@ -163,19 +200,19 @@ void translate_direct(directWord* word, char* label_name, int address, Shed* she
 void translate_indirect_register(registerWord* word, char* register_name, mode mode) {
     if (mode == SOURCE) {
         char* ptr = register_name;
-        word->are = 1;
+        word->are = 4;
         word->source_regiester_name = atoi((ptr+1));
     }
     else if (mode == DESTENATION) {
         char* ptr = register_name;
-        word->are = 1;
+        word->are = 4;
         word->destination_register_name = atoi((ptr+1));
     }
     return;
 }
 
 void translate_imidiate(imidiateWord* word, char* value) {
-    word->are = 1;
+    word->are = 4;
     word->value = atoi(value);
     return;
 }
@@ -188,15 +225,157 @@ void translate_label_defenition(ASTNode* node, Shed* shed) {
             memory_allocation_failure();
         }
 
-        translate_instruction_first(first_word, node, shed);
+        translate_instruction_first(&first_word->type.first_word, node, shed);
     }
 }
 
-void translate_directive (ASTNode* node)
+void translate_string_directive (ASTNode* node, Shed* shed) { 
+    char* ptr = node->data.directive.value;
+    directiveWord* directive_word;
+    for (; *(ptr) != '\0'; ptr++) {
+        directive_word = malloc(sizeof(directWord));
+        if (directive_word == NULL) {
+            memory_allocation_failure();
+        }
+        memset(directive_word, 0, sizeof(directiveWord));
+        directive_word->value = *ptr;
+        (*shed->dc)++;
+        print_directive_word(directive_word);
+    }
+    (*shed->dc)++;
+    directive_word->value = 0;
+    print_directive_word(directive_word);
+}
 
+void translate_data_directive(ASTNode* node, Shed* shed) {
+    char* ptr = node->data.directive.value;
+    for (; *(ptr) != '\0'; ptr++) {
+        directiveWord* directive_word;
+        if (*ptr == ',') continue;
+        directive_word = malloc(sizeof(directWord));
+        if (directive_word == NULL) {
+            memory_allocation_failure();
+        }
+        memset(directive_word, 0, sizeof(directiveWord));
+        /* remove atoi */
+        directive_word->value = atoi(ptr);
+        (*shed->dc)++;
+        print_directive_word(directive_word);
+    }
+}
 
-// Creata a function that can translate a directive
+void print_word(Word* word) {
+    switch (word->word_type) {
+        case FIRST_WORD: 
+            print_first_word(&word->type.first_word);
+        break;
+        case DIRECT_WORD:
+            print_direct_word(&word->type.direct_word);
+        break;
+        case IMIDIATE_WORD:
+            print_imidiate_word(&word->type.imidiate_word);
+        break;
+        case REGISTER_WORD:
+            print_register_word(&word->type.register_word);
+        break;
+        case DIRECTIVE_WORD:
 
-// Create a function that can translate a label
+    }
+}
+/*
+void print_first_word(firstWord* word) {
+    printf("Word: %u%u%u%u\n", word->opcode, word->source_address, word->destenation_address, word->are);
+}
 
-// Create a function that can send the output into the file 
+void print_direct_word(directWord* word) {
+    printf("Word: %u%u\n", word->address, word->are);
+} 
+
+void print_imidiate_word(imidiateWord* word) {
+    printf("Word: %u%u\n", word->value, word->are);
+}
+
+void print_register_word(registerWord* word) {
+    printf("Word: %u%u%u%u\n", word->padding, word->source_regiester_name, word->destination_register_name, word->are);
+}
+*/
+void translate(Shed* shed) {
+    while (shed->node_list->head != NULL) {
+        delegate_node(shed->node_list->head, shed->file_name, shed);
+        shed->node_list->head = shed->node_list->head->next;
+    }
+}
+
+void print_first_word(firstWord* word) {
+    int i;
+    printf("Word: ");
+    for (i = 3; i >= 0; --i) { 
+        printf("%u", (word->opcode >> i) & 1);
+    }
+    for (i = 3; i >= 0; --i) { 
+        printf("%u", (word->source_address >> i) & 1);
+    }
+    for (i = 3; i >= 0; --i) { 
+        printf("%u", (word->destenation_address >> i) & 1);
+    }
+    for (i = 2; i >= 0; --i) { 
+        printf("%u", (word->are >> i) & 1);
+    }
+    printf("\n");
+}
+
+void print_direct_word(directWord* word) {
+    int i;
+    printf("Word: ");
+    for (i = 11; i >= 0; --i) { 
+        printf("%u", (word->address >> i) & 1);
+    }
+    for (i = 2; i >= 0; --i) {
+        printf("%u", (word->are >> i) & 1);
+    }
+    printf("\n");
+} 
+
+void print_imidiate_word(imidiateWord* word) {
+    int i;
+    printf("Word: ");
+    for (i = 11; i >= 0; --i) { 
+        printf("%u", (word->value >> i) & 1);
+    }
+    for (i= 2; i >= 0; --i) { 
+        printf("%u", (word->are >> i) & 1);
+    }
+    printf("\n");
+}
+
+void print_register_word(registerWord* word) {
+    int i;
+    printf("Word: ");
+    for (i = 5; i >= 0; --i) {
+        printf("%u", (word->padding >> i) & 1);
+    }
+    for (i = 2; i >= 0; --i) { 
+        printf("%u", (word->source_regiester_name >> i) & 1);
+    }
+    for (i = 2; i >= 0; --i) { 
+        printf("%u", (word->destination_register_name >> i) & 1);
+    }
+    for (i = 2; i >= 0; --i) { 
+        printf("%u", (word->are >> i) & 1);
+    }
+    printf("\n");
+}
+
+void print_directive_word(directiveWord* word) {
+    int i;
+    printf("Word: ");
+    for (i = 14; i >= 0; --i) {
+        printf("%u", (word->value >> i) & 1);
+    }
+    printf("\n");
+}
+/* Creata a function that can translate a directive */
+ 
+/* Create a function that can translate a label */
+
+/* Create a function that can send the output into the file  */
--- transletor.h
+++ transletor.h
@@ -7,29 +7,37 @@
 #include "panic.h"
 #include "symbol_table.h"
 
+typedef enum {
+    FIRST_WORD, DIRECT_WORD, IMIDIATE_WORD, REGISTER_WORD, DIRECTIVE_WORD
+} wordType;
+
 typedef struct {
-    uint16_t are : 3;
-    uint16_t destenation_address : 4;
-    uint16_t source_address : 4;
-    uint16_t opcode : 4;
+    unsigned int are : 3;
+    unsigned int destenation_address : 4;
+    unsigned int source_address : 4;
+    unsigned int opcode : 4;
 } firstWord;
 
 typedef struct {
-    uint16_t are : 3;
-    uint16_t address : 12;
+    unsigned int are : 3;
+    unsigned int address : 12;
 } directWord;
 
 typedef struct {
-    uint16_t are : 3;
-    uint16_t value : 12;
+    unsigned int are : 3;
+    unsigned int value : 12;
 } imidiateWord;
 
 typedef struct {
-    uint16_t are : 3;
-    uint16_t source_regiester_name : 3;
-    uint16_t destination_register_name : 3;
-    uint16_t padding : 6; 
+    unsigned int are : 3;
+    unsigned int source_regiester_name : 3;
+    unsigned int destination_register_name : 3;
+    unsigned int padding : 6; 
 } registerWord;
+
+typedef struct {
+    unsigned int value : 15;
+} directiveWord;
 
 typedef struct {
     union{
@@ -37,7 +45,9 @@ typedef struct {
         directWord direct_word;
         imidiateWord imidiate_word;
         registerWord register_word;
+        directWord directive_word;
     } type;
+    wordType word_type;
 } Word;
 
 typedef struct {
@@ -47,6 +57,9 @@ typedef struct {
     directWord* direct_word;
     imidiateWord* imidiate_word;
     registerWord* register_word;
+    ASTNodeList* node_list;
+    int* ic;
+    int* dc;
 } Shed;
 
 typedef enum {
@@ -59,5 +72,16 @@ void translate_indirect_register(registerWord* word, char* register_name, mode m
 void translate_direct(directWord* word, char* label_name, int address, Shed* shed);
 void translate_imidiate(imidiateWord* word, char* value); 
 void handle_second_word(ASTNode* node, int source_addressing_mode, int destenation_addressing_mode, Shed* shed); 
+void delegate_node(ASTNode* node, char* file_name, Shed* shed);
+void print_first_word(firstWord* word);
+void print_direct_word(directWord* word);
+void print_imidiate_word(imidiateWord* word); 
+void print_register_word(registerWord* word); 
+firstWord* translate_instruction_first(firstWord* first_word ,ASTNode* node, Shed* shed);
+void translate(Shed* shed);
+void print_directive_word(directiveWord* word); 
+void translate_string_directive (ASTNode* node, Shed* shed);
+void translate_data_directive(ASTNode* node, Shed* shed); 
+
 
 #endif
