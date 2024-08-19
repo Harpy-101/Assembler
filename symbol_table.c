f68ee851f61b2df5f8dd305f241e407e0d3e2908
David Israel <davidinp6@gmail.com>
2024-08-20 00:07:19 +0300

    Added support for a directives hash-table and updated the functions to fit the new build

--- symbol_table.c
+++ symbol_table.c
@@ -1,4 +1,6 @@
 #include "symbol_table.h"
+#include "panic.h"
+/*#include "transletor.h"*/
 #include <stdio.h>
 #include <string.h>
 
@@ -9,26 +11,38 @@ unsigned int hash(char *s, int hashsize) {
     return hashval % hashsize;
 }
 
-HashTable* create_hash_table(int size) {
-    HashTable* hash_table = (HashTable*)malloc(sizeof(HashTable));
+SymbolTable* create_hash_table(int size) {
+    SymbolTable* hash_table = (SymbolTable*)malloc(sizeof(SymbolTable));
     if (hash_table == NULL) {
-        printf("Memory allocation for hash table failed\n");
-        exit(EXIT_FAILURE);
+        memory_allocation_failure();
     }
 
     hash_table->size = size;
     hash_table->count = 0;
     hash_table->map = (Symbol**)calloc(size, sizeof(Symbol*));
     if (hash_table->map == NULL) {
-        printf( "Memory allocation for hash table map failed\n");
-        free(hash_table);
-        exit(EXIT_FAILURE);
+        memory_allocation_failure();
     }
 
     return hash_table;
 }
 
-void insert_symbol(HashTable* hash_table, char* name, int line_address) {
+DirectiveTable* create_directive_table(int size) {
+    DirectiveTable* directive_table = (DirectiveTable*)malloc(sizeof(DirectiveTable));
+    if (directive_table == NULL) {
+        memory_allocation_failure();
+    }
+
+    directive_table->size = size;
+    directive_table->count = 0;
+    directive_table->map = (directiveRef**)calloc(size, sizeof(directiveRef*));
+    if (directive_table->map == NULL) {
+        memory_allocation_failure();
+    }
+    return directive_table;
+}
+
+void insert_symbol(SymbolTable* hash_table, char* name, int line_address) {
     int hash_index;
     Symbol* symbol;
 
@@ -38,27 +52,50 @@ void insert_symbol(HashTable* hash_table, char* name, int line_address) {
 
     symbol = (Symbol*)malloc(sizeof(Symbol));
     if (symbol == NULL) {
-        printf("Memory alocation failed\n");
-        exit(EXIT_FAILURE);
+        memory_allocation_failure();
     }
 
     symbol->name = strdup(name);
     if (symbol->name == NULL) {
-        printf("Memory alocation for name failed\n");
-        free(symbol);
-        exit(EXIT_FAILURE);
+       memory_allocation_failure();
     }
 
-    symbol->address = line_address;
+    symbol->address = &line_address;
     symbol->name = name;
     hash_index = hash(name, hash_table->size);
     symbol->next = hash_table->map[hash_index];
     hash_table->map[hash_index] = symbol;
     hash_table->count++;
-    printf("Inserted symbol: %s at address: 0x%x in hash index: %d\n", symbol->name, symbol->address, hash_index);
+    printf("Inserted symbol: %s at address: 0x%x in hash index: %d\n", symbol->name, *symbol->address, hash_index);
 }
 
-Symbol* lookup_symbol(HashTable* hash_table, char* name) {
+void insert_directive(DirectiveTable* directive_table, char* name, int directive_type) {
+    int hash_index;
+    directiveRef* directive;
+
+    if ((float)directive_table->count / directive_table->size > LOAD_FACTOR) {
+        resize_directive_table(directive_table);
+    }
+
+    directive = (directiveRef*)malloc(sizeof(directiveRef));
+    if (directive == NULL) {
+        memory_allocation_failure();
+    }
+
+    directive->name = strdup(name);
+    if (directive->name == NULL) {
+        memory_allocation_failure();
+    }
+
+    directive->directive_type = directive_type;
+    hash_index = hash(name, directive_table->size);
+    directive->next = directive_table->map[hash_index];
+    directive_table->map[hash_index] = directive;
+    directive_table->count++;
+    printf("Inserted directive: %s in hash index: %d\n", directive->name, hash_index);
+}
+
+Symbol* lookup_symbol(SymbolTable* hash_table, char* name) {
     int hash_index = hash(name, hash_table->size);
     Symbol* symbol = hash_table->map[hash_index];
     
@@ -71,7 +108,20 @@ Symbol* lookup_symbol(HashTable* hash_table, char* name) {
     return NULL;
 }
 
-void clean_symbol_table(HashTable* hash_table) {
+directiveRef* lookup_directive(DirectiveTable* directive_table, char* name) {
+    int hash_index = hash(name, directive_table->size);
+    directiveRef* directive = directive_table->map[hash_index];
+    
+    while (directive != NULL) {
+        if (strcmp(directive->name, name) == 0) {
+            return directive;
+        }
+        directive = directive->next;
+    }
+    return NULL;
+}
+
+void clean_symbol_table(SymbolTable* hash_table) {
     int i;
     for (i = 0; i < hash_table->size; i++) {
         Symbol* symbol = hash_table->map[i];
@@ -86,16 +136,30 @@ void clean_symbol_table(HashTable* hash_table) {
     free(hash_table);
 }
 
+void clean_directive_table(DirectiveTable* directive_table) {
+    int i;
+    for (i = 0; i < directive_table->size; i++) {
+        directiveRef* directive = directive_table->map[i];
+        while (directive != NULL) {
+            directiveRef* temp = directive;
+            directive = directive->next;
+            free(temp->name);
+            free(temp);
+        }
+    }
+    free(directive_table->map);
+    free(directive_table);
+}
+
 /* Still need to check this function. 
    It's possible that something in the whiile loop is wrong, but I'm not sure
 */
-void resize_hash_table(HashTable* hash_table) {
+void resize_hash_table(SymbolTable* hash_table) {
     int i;
     int new_size = hash_table->size * 2;
-    Symbol** new_map = malloc(new_size * sizeof(HashTable));
+    Symbol** new_map = malloc(new_size * sizeof(SymbolTable));
     if (new_map == NULL) {
-        printf("Memory alocation for resizing of the hashtable falied\n");
-        exit(EXIT_FAILURE);
+        memory_allocation_failure();
     }
     
     for (i = 0; i < new_size; i++) {
@@ -116,28 +180,62 @@ void resize_hash_table(HashTable* hash_table) {
     hash_table->size = new_size;
 }
 
+void resize_directive_table(DirectiveTable* hash_table) {
+    int i;
+    int new_size = hash_table->size * 2;
+    directiveRef** new_map = malloc(new_size * sizeof(DirectiveTable));
+    if (new_map == NULL) {
+        memory_allocation_failure();
+    }
+    
+    for (i = 0; i < new_size; i++) {
+        new_map[i] = NULL;
+    }
+
+    for (i = 0; i < hash_table->size; i++) {
+        directiveRef* directive = hash_table->map[i];
+        while (directive) {
+            directiveRef* new_directive = directive->next;
+            unsigned int index = hash(directive->name, new_size);
+            new_directive->next = new_map[index];
+            new_map[index] = new_directive;
+        }
+    }
+    free(hash_table->map);
+    hash_table->map = new_map;
+    hash_table->size = new_size;
+}
+
 unresolvedLabelRefList* create_unresolved_label_list() {
     unresolvedLabelRefList* unresolved_list = malloc(sizeof(unresolvedLabelRefList));
     if (unresolved_list == NULL) {
-        printf("Memory alocation for \"unresolved list\" falied\n");
-        exit(EXIT_FAILURE);
+        memory_allocation_failure();
     }
     unresolved_list->head = NULL;
     return unresolved_list;
 }
 
-void add_unresolved_label(char* name, int address, unresolvedLabelRefList* unresolved_list, char* file_name) {
+void add_unresolved_label(char* name, int* address, Word* word, unresolvedLabelRefList* unresolved_list) {
     unresolvedLabelRef* label = malloc(sizeof(unresolvedLabelRef));
    if (label == NULL) {
-        printf("Memory alocation for \"unresolved label\" falied\n");
-        exit(EXIT_FAILURE);
+        memory_allocation_failure();
     }
     label->name = strdup(name); 
-    /* Test that the alocation succeeded */
-    label->origin = strdup(file_name);
+    if (label->name == NULL) {
+        memory_allocation_failure();
+    }
     label->address = address;
+    label->word = word;
     label->next = unresolved_list->head;
     unresolved_list->head = label;
+}
+
+void clear_unresolved_list(unresolvedLabelRefList* list) {
+    unresolvedLabelRef* node = list->head;
+    while (node) {
+        free(node->name);
+        node = node->next;
+    }
 }
 
 char* strdup(const char* src) {
@@ -158,14 +256,14 @@ char* strdup(const char* src) {
     return dup;
 }
 
-void print_symbol_table(HashTable* symbol_table) {
+void print_symbol_table(SymbolTable* symbol_table) {
     int i;
     for (i = 0; i < symbol_table->size; i++) {
         Symbol* curr_symbol = symbol_table->map[i];
         if (curr_symbol) {
             printf("Index: %d\n", i);
             while (curr_symbol) {
-                printf("Symbol: %s, found in line: %d\n", curr_symbol->name, curr_symbol->address);
+                printf("Symbol: %s, found in line: %d\n", curr_symbol->name, *curr_symbol->address);
                 curr_symbol = curr_symbol->next;
             }
         }
@@ -175,7 +273,7 @@ void print_symbol_table(HashTable* symbol_table) {
 void print_unresolved_list(unresolvedLabelRefList* list) {
     unresolvedLabelRef* node = list->head;
     while (node != NULL) {
-        printf("Name: %s, at line: %d\n", node->name, node->address);
+        printf("Name: %s, at line: %d\n", node->name, *node->address);
         node = node->next;
     }
 }
@@ -188,7 +286,7 @@ void print_unresolved_list(unresolvedLabelRefList* list) {
  * @return :
  * todo: Add a proper resolution algorithem for both directives and labels. 
  */
-void resolve_unresolved_list(HashTable* symbol_table, unresolvedLabelRefList* list) {
+/*void resolve_unresolved_list(SymbolTable* symbol_table, unresolvedLabelRefList* list) {
     unresolvedLabelRef* prev = NULL;
     unresolvedLabelRef* node = list->head;
     Symbol* found;
@@ -213,4 +311,30 @@ void resolve_unresolved_list(HashTable* symbol_table, unresolvedLabelRefList* li
             free(node);
         }
     }
+}*/
+/*
+HashTable* create_hash_table(
+                            int size,
+                            void* (lookup_function)(HashTable* table, char* value),
+                            int (*add_function)(HashTable* table, char* value, int address),
+                            void (*clear_function)(void* data)) {
+    HashTable* hash_table = (HashTable*)malloc(sizeof(HashTable));
+    if (hash_table == NULL) {
+        memory_allocation_failure();
+    }
+
+    hash_table->size = size;
+    hash_table->count = 0;
+    hash_table->map = (void**)calloc(size, sizeof(void*));
+    if (hash_table->map == NULL) {
+        free(hash_table);
+        memory_allocation_failure();
+    }
+
+    hash_table->lookup_function = lookup_function;
+    hash_table->add_function = add_function;
+    hash_table->clear_function = clear_function;
+    
+    return hash_table;
 }
+*/
--- symbol_table.h
+++ symbol_table.h
@@ -4,10 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
+#include "shed.h"
+#include "word_types.h"
+
 
 #define INITIAL_HASH_TABLE_SIZE 100
 #define LOAD_FACTOR 0.8
+
 
 struct ASTNode;
 
@@ -15,26 +18,51 @@ typedef enum {
     EXTERN, ENTRY, UNDEFINED
 } symbol_scope;
 
+typedef enum {
+    EXTERN_DIRECTIVE, ENTRY_DIRECTTIVE
+} diretiveType;
+
+typedef enum {
+    IC, DC
+} address_type;
+
 typedef struct Symbol {
     char* name;
-    int address;
-    symbol_scope scope;
+    address_type address_type;
+    int* address;
     char* definition_origin;    
     struct Symbol* next;
     struct ASTNode* dfinition_node;
 } Symbol;
+/*
+typedef struct HashTable{
+    void** map;
+    int size;
+    int count;
+    int (*add_function)(struct HashTable* table, char* value, int address);
+    void* (*lookup_function)(struct HashTable* table, char* value);
+    void (*clear_function)(void* data);
+} HashTable;
+*/
 
 typedef struct {
     Symbol **map;
     int size;
     int count;
-} HashTable;
+} SymbolTable;
+
+typedef struct {
+    unsigned int are : 3;
+    unsigned int address : 12; 
+} labelWord;
 
 typedef struct unresolvedLabelRef {
     char* name;
-    int address;
+    int* address;
+    address_type ad_type;
     symbol_scope scope;
     char* origin;
+    Word* word;
     struct unresolvedLabelRef* next;
 } unresolvedLabelRef;
 
@@ -42,19 +70,38 @@ typedef struct {
     unresolvedLabelRef* head;
 } unresolvedLabelRefList;
 
-extern HashTable* symbol_table;
+typedef struct directiveRef{
+    char* name;
+    diretiveType directive_type; 
+    struct directiveRef* next;
+} directiveRef;
+
+typedef struct {
+    directiveRef **map;
+    int size;
+    int count;
+} DirectiveTable;
+
 
 unsigned int hash(char *s, int hashsize);
-HashTable* create_hash_table(int table_size);
-void insert_symbol(HashTable* hash_table, char* name, int line_address);
-Symbol* lookup_symbol(HashTable* hash_table, char* name);
-void clean_symbol_table(HashTable* hash_table); 
-void resize_hash_table(HashTable* hash_table);
+SymbolTable* create_hash_table(int table_size);
+void insert_symbol(SymbolTable* hash_table, char* name, int line_address);
+Symbol* lookup_symbol(SymbolTable* hash_table, char* name);
+void clean_symbol_table(SymbolTable* hash_table); 
+void resize_hash_table(SymbolTable* hash_table);
 unresolvedLabelRefList* create_unresolved_label_list();
-void add_unresolved_label(char* name, int address, unresolvedLabelRefList* unresolved_list, char* file_name);
+void add_unresolved_label(char* name, int* address, Word* word, unresolvedLabelRefList* unresolved_list);
 char* strdup(const char* src); 
-void print_symbol_table(HashTable* symbol_table); 
+void print_symbol_table(SymbolTable* symbol_table); 
 void print_unresolved_list(unresolvedLabelRefList* list);
-void resolve_unresolved_list(HashTable* symbol_table, unresolvedLabelRefList* list); 
+/*void resolve_unresolved_list(HashTable* symbol_table, unresolvedLabelRefList* list); */
+DirectiveTable* create_directive_table(int size);
+void resize_directive_table(DirectiveTable* hash_table);
+void clean_directive_table(DirectiveTable* directive_table);
+directiveRef* lookup_directive(DirectiveTable* directive_table, char* name);
+void insert_directive(DirectiveTable* directive_table, char* name, int directive_type);
+void clear_unresolved_list(unresolvedLabelRefList* list);
+
+
 
 #endif
